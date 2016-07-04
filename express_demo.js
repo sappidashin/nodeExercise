110751da0f475995b2b59206866c67dc02c81279
sappidashin <johnny.yeh@sapphireinfo.com.tw>
2016-07-04 15:31:17 +0800

    express_demo
    express_demo step2

--- express_demo.js
+++ express_demo.js
@@ -2,7 +2,23 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-	res.send('Hello World');
+	console.log('visit via Get');
+	res.send('Hello GET');
+})
+
+app.post('/', function(req, res){
+	console.log('visit via Post');
+	res.send('Hello POST');
+})
+
+app.delete('/del_user', function(req, res){
+	console.log('/del_user delete request');
+	res.send('delete page');
+})
+
+app.get('/list_user', function(req, res){
+	console.log('/list_user GET request');
+	res.send('display user list');
 })
 
 var server = app.listen(8081, function(){
